1e04933f4cb783dc2015bfc918d4d56eaf546c77
meepawn <bluenism@yahoo.com>
2016-05-14 20:00:56 +0700

    First

---
+++ Tinkerdb/app/src/main/java/art2/tinkerdb/MainActivity.java
@@ -0,0 +1,13 @@
+package art2.tinkerdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
